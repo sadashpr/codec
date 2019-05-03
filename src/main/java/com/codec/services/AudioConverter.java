ac242a311261273fddb811a4a931efb7154f6b13
psadashiv <pradeep.sadashiv@bulletproof.net>
2019-05-03 20:47:38 +1000

    added ffmpeg

--- src/main/java/com/codec/services/AudioConverter.java
+++ src/main/java/com/codec/services/AudioConverter.java
@@ -15,45 +15,39 @@ public class AudioConverter{
 public static void convertFile(String fileName)
 {
     try{
-       // String command = "ffmpeg -i "+fileName+ " "+fileName+"_new" ;
-       String command = "ffprobe " +fileName;
-        //String[] command = {"cat", "/Users/pradeep.sadashiv/Documents/test/assignment_files/abcd.txt", ">>", "/Users/pradeep.sadashiv/Documents/test/assignment_files/hello.txt"};
-//String command = "cat /Users/pradeep.sadashiv/Documents/test/assignment_files/abcd.txt";
-        System.out.println(command);
-        Process proc=Runtime.getRuntime().exec(command);
+       String command1 = "ffprobe " +fileName;
+        Process proc=Runtime.getRuntime().exec(command1);
         //proc.waitFor();
         BufferedReader stdInput = new BufferedReader(new 
-     InputStreamReader(proc.getInputStream()));
+        InputStreamReader(proc.getInputStream()));
 
-BufferedReader stdError = new BufferedReader(new 
-     InputStreamReader(proc.getErrorStream()));
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
 
-// read the output from the command
-//System.out.println("Here is the standard output of the command:\n");
-String s = null;
-while ((s = stdInput.readLine()) != null) {
-   // System.out.println(s);
-}
-String output = new String();
-//System.out.println("Here is the standard error of the command (if any):\n");
-
-//why the hell is valid output coming in stderror!! 
-while ((s = stdError.readLine()) != null) {
-    if(s.contains("(eng)"))
-    {
-        int startIndex = s.indexOf("(eng)") - 3 ;
-        int endIndex = startIndex+3;
-        String replacement = "";
-        replacement= s.substring(startIndex,endIndex);
-        output = output+" -map "+replacement;
-    }
+        // read the output from the command1
+        String s = null;
+        while ((s = stdInput.readLine()) != null) {
+        // System.out.println(s);
+        }
+        String output = new String();
+        //why the hell is valid output coming in stderror!! 
+        while ((s = stdError.readLine()) != null) {
+            if(s.contains("(eng)"))
+            {
+                //parse output selectively to extract mapping values for english.
+                int startIndex = s.indexOf("(eng)") - 3 ;  
+                int endIndex = startIndex+3;
+                String replacement = "";
+                replacement= s.substring(startIndex,endIndex);
+                output = output+" -map "+replacement;
+            }
 }
 
-String command2  = "ffmpeg -i "+ fileName + " -c copy "+ output + " /Users/pradeep.sadashiv/Documents/test/temp/output.mov";
-System.out.println("printing output");
-System.out.println(command2);
-
-
+            String newFilename = getOutPutFilename(fileName);
+            //create 2nd command to convert files.
+            String command2  = "ffmpeg -i "+ fileName + " -c copy "+ output + " "+newFilename;
+            System.out.println("printing output");
+            System.out.println(command2);
+            Process proc2=Runtime.getRuntime().exec(command2);
     }
     catch(java.io.IOException e){
          System.out.println("error happened");
@@ -64,5 +58,13 @@ public static void convertFiles(List<String> fileNames)
 {
     for(String file: fileNames)
        AudioConverter.convertFile(file);
+}
+
+public static String getOutPutFilename(String origFilename){
+
+    StringBuffer newFilename = new StringBuffer(origFilename); 
+    int index = origFilename.lastIndexOf("/");
+    newFilename.insert(index + 1, "stripped_"); 
+    return newFilename.toString();
 }
 }
